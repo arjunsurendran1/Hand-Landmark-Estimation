a697a1cb5c4b1019eb6af43788eef987affccbe8
arjunsurendran1 <arjunsurendran1234@gmail.com>
2023-05-25 10:45:03 +0530

    final commit

---
+++ hand_tracking.py
@@ -0,0 +1,49 @@
+#importing libraries
+import cv2
+import mediapipe as mp
+import time
+
+
+cap = cv2.VideoCapture(0)
+
+#creating objects
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+#for fps
+pTime = 0
+cTime=0
+
+
+while True:
+    success,img = cap.read()
+
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                #print(id,lm)
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
+                '''
+                if id==0:
+                    cv2.circle(img,(cx,cy),25,(250,0,255),cv2.FILLED)'''
+
+            #draw landmarks
+            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+
+
+    cTime = time.time()
+    #calculate fps
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    #show fps
+    cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+
+
+    cv2.imshow('image',img)
+    cv2.waitKey(1)
